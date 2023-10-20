a819b994bdebd7869265f7bfe0b6103b9ba7aa63
Pathompong Jiamjultikul <115784602+Skitter016@users.noreply.github.com>
2023-10-20 16:00:56 +0700

    G

---
+++ temp/models/products.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+
+const Product = sequelize.define('Product', {
+  name: {
+    type: DataTypes.STRING
+  },
+  country: {
+    type: DataTypes.STRING
+  },
+  temp: {
+    type: DataTypes.STRING
+  },
+  weather: {
+    type: DataTypes.STRING
+  },
+  humidity: {
+    type: DataTypes.STRING
+  },
+  pressure: {
+    type: DataTypes.STRING
+  },
+  speed: {
+    type: DataTypes.STRING
+  },
+  
+});
+
+module.exports = Product;
